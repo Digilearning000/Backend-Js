15d4a6eac21d82dca6e08e32bdf86394dc7074a8
Digilearning000 <digilearning000@gmail.com>
2024-05-06 15:06:39 -0400

    Dataabse connected

---
+++ src/db/index.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+import { DB_NAME } from "../constant.js";
+
+const connectDb = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+    console.log(`MongoDb connected : ${connectionInstance.connection.host}`);
+  } catch (error) {
+    console.log(`MongoDb Connection Error :`, error);
+    process.exit(1);
+  }
+};
+
+export default connectDb;
